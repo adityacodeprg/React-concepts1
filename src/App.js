294664537f1617484519072f56ce71ab44be7ff0
Aditya Dev <aditya.depal@gmail.com>
2023-06-16 13:12:41 -0500

    webpage implmentation

--- src/App.js
+++ src/App.js
@@ -1,38 +1,82 @@
-import './App.css';
-import './components/ExpenseItem';
-import ExpenseItem from './components/ExpenseItem';
-import Expenses from './components/Expenses';
+import componentsImage from './assests/images/components.png'
+import stateImage from './assests/images/state.png'
+import eventsImage from './assests/images/events.png'
+import Header from './components/Header/Header.js';
+import Concept from './components/Concept/Concept.js';
 
+const concepts = [
+{
+  title: 'Components',
+  image: componentsImage,
+  description:
+    'Components let you split the UI into independent, reusable pieces, and think about each piece in isolation. Components can receive data via props, and they can render dynamic output using JSX.',
+},
+{
+  title: 'State',
+  image: stateImage,
+  description:
+    'State is data that may change over time. As it changes, the UI should be updated to reflect the updated data. Each component can maintain its own state and multiple components can share state.',
+},
+{
+  title: 'Events',
+  image: eventsImage,
+  description:
+    'Event handlers are added via props to (built-in) components. You pass functions as values to such event handlers to control which functions gets executed for which event.',
+},
+];
+
+//normal approach
+// function App() {
+//   return (
+//     <div>
+//       <Header />
+//       <ul id="concepts">
+//         <Concept
+//           image={concepts[0].image}
+//           title={concepts[0].title}
+//           description={concepts[0].description}
+//         />
+//         <Concept
+//           image={concepts[1].image}
+//           title={concepts[1].title}
+//           description={concepts[1].description}
+//         />
+//         <Concept
+//           image={concepts[2].image}
+//           title={concepts[2].title}
+//           description={concepts[2].description}
+//         />
+//       </ul>
+//     </div>
+//   );
+// }
+
+//better apprach using map method, assign key prop to each concept component
 function App() {
-  const expenses = [
-    {
-      id: 'e1',
-      title: 'Toilet Paper',
-      amount: 94.12,
-      date: new Date(2020, 7, 14),
-    },
-    { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) },
-    {
-      id: 'e3',
-      title: 'Car Insurance',
-      amount: 294.67,
-      date: new Date(2021, 2, 28),
-    },
-    {
-      id: 'e4',
-      title: 'New Desk (Wooden)',
-      amount: 450,
-      date: new Date(2021, 5, 12),
-    },
-  ];
-
-  return (
+  return(
     <div>
-      <h2>Let's get started!</h2>
-      <Expenses items={expenses} />
+<Header />
+<ul id="concepts">
+  {concepts.map((concept) => (
+<Concept
+  key={concept.title}
+  image={concept.image}
+  title={concept.title}
+  description={concept.description}
+  />
+  ))}
+  </ul>
     </div>
   );
 }
 
-export default App;
 
+
+
+
+
+
+
+
+
+export default App;
